87e9ed39f466792d19c2837bdc6483e358855f3f
Erik <esundbla@msudenver.edu>
2022-06-21 15:29:09 -0600

    js db basics

--- db_basics.js
+++ db_basics.js
@@ -1,9 +1,11 @@
-var mongo = require('mongodb')
+var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
 var url = "mongodb://localhost:27017/SME";
+
+const Simon = ["Jordyn.Corenman", "Jarred.Mckay", "Chris.Nodine", "Erik.Sundblad"];
 var doc ={
-  "_id": 1,
+  //"_id": 1,
   "Overall" : "Content and lab dev",
   "Sub-Cat" : "Assist in building lab enviroments",
   "metric" : "# of others using labs",
@@ -11,15 +13,49 @@ var doc ={
   "Proof" : "pdf_path",
   "Notes" : "This would be for notes on the file",
   "Tag" : "Azure"
-}
+};
+var query = {"Sub-Cat":"Assist in building lab enviroments"}
 
+/*
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var emps = db.db()
-  emps.collection("erik.sundblad").insertOne(doc, function(err, res){
+  var emps = db.db();
+  for (let emp in Simon){
+    emps.collection(Simon[emp]).insertOne(doc, function(err, res){
+      if (err) throw err;
+      console.log("Inserted Doc");
+      db.close();
+    });
+  };
+});
+*/
+
+
+/*
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var emps = db.db();
+  emps.collection("Erik.Sundblad").find(query).toArray(function(err, result) {
     if (err) throw err;
-    console.log("Inserted Doc");
+    for (let item in result){
+      console.log(result[item]);
+    };
     db.close();
   });
 });
+*/
 
+MongoClient.connect(url).then((client) => {
+  const connect = client.db()
+  connect.listCollections().toArray(function(err, names) {   
+      if(!err) {
+        for (let item in names){
+          console.log(names[item])
+      };
+    }
+  });
+}).catch((err) => {
+
+  // Printing the error message
+  console.log(err.Message);
+})
