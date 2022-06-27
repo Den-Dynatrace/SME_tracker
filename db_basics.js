8cca180f37150d1aa42bc68d84b9cd4d22d25a5f
Erik <esundbla@msudenver.edu>
2022-06-27 16:06:07 -0600

    passport server intro

--- db_basics.js
+++ db_basics.js
@@ -1,38 +1,93 @@
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
-//Create a database named "mydb":
 var url = "mongodb://localhost:27017/SME";
 
-const Simon = ["Jordyn.Corenman", "Jarred.Mckay", "Chris.Nodine", "Erik.Sundblad"];
-var doc ={
-  //"_id": 1,
-  "Overall" : "Content and lab dev",
-  "Sub-Cat" : "Assist in building lab enviroments",
-  "metric" : "# of others using labs",
-  "value" : 6,
+testing_eriks_docs = [
+  {"Overall" : "Content and lab dev",
+  "Sub-Cat" : "Partners to dev best practices",
+  "metric" : "# of training deliverd",
+  "value" : 1,
   "Proof" : "pdf_path",
   "Notes" : "This would be for notes on the file",
   "Tag" : "Azure"
-};
-var query = {"Sub-Cat":"Assist in building lab enviroments"}
+  },
+  {
+    "Overall" : "Content and lab dev",
+  "Sub-Cat" : "Assist in building lab enviroments",
+  "metric" : "# of others using labs",
+  "value" : 1,
+  "Proof" : "pdf_path",
+  "Notes" : "This would be for notes on the file",
+  "Tag" : "Azure"
+  },
+  {
+    "Overall" : "Content and lab dev",
+  "Sub-Cat" : "Assist in building lab enviroments",
+  "metric" : "# of lab environments developed",
+  "value" : 1,
+  "Proof" : "pdf_path",
+  "Notes" : "This would be for notes on the file",
+  "Tag" : "AWS"
+  },
+  {"Overall" : "D1 representative/evangelist (internal and external)",
+  "Sub-Cat" : "Participate in cross-functional “SWAT”, Value Creation, etc. teams",
+  "metric" : "# of groups represented in",
+  "value" : 1,
+  "Proof" : "pdf_path",
+  "Notes" : "This would be for notes on the file",
+  "Tag" : "Azure"
+  },
+  {"Overall" : "Recognition & impact",
+  "Sub-Cat" : "Continue to expand technical knowledge and stay ahead of industry trends",
+  "metric" : "# of advanced certs",
+  "value" : 1,
+  "Proof" : "pdf_path",
+  "Notes" : "This would be for notes on the file",
+  "Tag" : "Azure"
+}
+]
 
-/*
+//INJECT ONE//
 MongoClient.connect(url, function(err, db) {
+  const Simon = ["Jordyn.Corenman", "Jarred.Mckay", "Chris.Nodine", "Erik.Sundblad"];
+  var doc ={
+  //"_id":, 
+  "Overall" : "Content and lab dev",
+  "Sub-Cat" : "Assist in building lab enviroments",
+  "metric" : "# of others using labs",
+  "value" : 1,
+  "Proof" : "pdf_path",
+  "Notes" : "This would be for notes on the file",
+  "Tag" : "Azure"
+  };
+
+  var id_doc = {
+    "_id" : "Sundblad, Erik",
+    "Department" : "Dynatrace ONE",
+    "Position": "Company Intern",
+    " Number" : 305576
+  }
+
   if (err) throw err;
   var emps = db.db();
-  for (let emp in Simon){
+
+  emps.collection("Erik.Sundblad").insertMany(testing_eriks_docs, function(err, res){
+    if (err) throw err;
+    console.log("Inserted Docs");
+  });
+  /*for (let emp in Simon){
     emps.collection(Simon[emp]).insertOne(doc, function(err, res){
       if (err) throw err;
       console.log("Inserted Doc");
       db.close();
     });
-  };
+  };*/
 });
-*/
-
 
 /*
+//SIMPLE QUERY//
 MongoClient.connect(url, function(err, db) {
+  var query = {"Sub-Cat":"Assist in building lab enviroments"}
   if (err) throw err;
   var emps = db.db();
   emps.collection("Erik.Sundblad").find(query).toArray(function(err, result) {
@@ -44,9 +99,8 @@ MongoClient.connect(url, function(err, db) {
   });
 });
 */
-
-
 /*
+//COLLECTION NAMES//
 MongoClient.connect(url, function(err, db) {
   const connect = db.db()
   connect.listCollections().toArray(function(err, names) {   
@@ -57,5 +111,10 @@ MongoClient.connect(url, function(err, db) {
     });
   });
 */
-
-
+/*
+//TAG QUERY//
+MongoClient.connect(url, function(err, db) {
+  const connect = db.db()
+  connect.collection
+});
+*/
